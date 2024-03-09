d8f69db0dfaed540477a725c3acab742f37b2821
unknown <ccao@oberlin.edu>
2024-03-09 15:37:52 -0500

    92

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -12,10 +12,10 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """Test commands"""
+    """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
-        """Test waiting for database if database ready"""
+        """Test waiting for database if database ready."""
         patched_check.return_value = True
 
         call_command('wait_for_db')
