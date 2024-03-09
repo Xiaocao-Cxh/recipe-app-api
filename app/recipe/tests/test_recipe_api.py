0b733a36e1c374fbce03dd4e965900afb488c255
unknown <ccao@oberlin.edu>
2024-03-09 16:07:08 -0500

    92

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -12,7 +12,7 @@ from rest_framework.test import APIClient
 
 from core.models import (
     Recipe,
-    Tag,
+    # Tag,
 )
 
 from recipe.serializers import (
@@ -186,7 +186,7 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Recipe.objects.filter(id=recipe.id).exists())
 
-    def test_recipe_other_users_recipe_error(self):
+    def test_delete_other_users_recipe_error(self):
         """Test trying to delete another users recipe gives error."""
         new_user = create_user(email='user2@example.com', password='test123')
         recipe = create_recipe(user=new_user)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -3,14 +3,14 @@ Views for the recipe APIs
 """
 from rest_framework import (
     viewsets,
-    mixins,
+    # mixins,
 )
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import (
     Recipe,
-    Tag,
+    # Tag,
 )
 from recipe import serializers
 
@@ -38,16 +38,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(# mixins.DestroyModelMixin,
-                 # mixins.UpdateModelMixin,
-                 mixins.ListModelMixin,
-                 viewsets.GenericViewSet):
-    """Manage tags in the database."""
-    serializer_class = serializers.TagSerializer
-    queryset = Tag.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+# class TagViewSet(# mixins.DestroyModelMixin,
+#                  # mixins.UpdateModelMixin,
+#                  mixins.ListModelMixin,
+#                  viewsets.GenericViewSet):
+#     """Manage tags in the database."""
+#     serializer_class = serializers.TagSerializer
+#     queryset = Tag.objects.all()
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        """Filter query set to authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+#     def get_queryset(self):
+#         """Filter queryset to authenticated user."""
+#         return self.queryset.filter(user=self.request.user).order_by('-name')
