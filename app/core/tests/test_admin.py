218de5ace6bb52de7b97c2f52be8d35988995473
unknown <ccao@oberlin.edu>
2024-03-09 14:57:30 -0500

    92

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -32,7 +32,7 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-    def test_user_change_page(self):
+    def test_edit_user_page(self):
         """Test that user edit page works."""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
